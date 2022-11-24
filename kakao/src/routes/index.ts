26facc9342186d36574b5c1e77648e77035bb53e
jeangho293 <jeangho293@naver.com>
2022-11-25 03:18:02 +0900

    주석 추가

--- kakao/src/routes/index.ts
+++ kakao/src/routes/index.ts
@@ -16,8 +16,11 @@ router.get('/kakao/login', async (req, res) => {
       responseType: "arraybuffer",
       decompress: true
     });
+
+  // NOTE: nodeJS로 받아오는 response을 받아오는 경우, gzip decompress 작업을 직접해줘야햐므로 zlib 사용.
   zlib.gunzip(data.data, (error, result) => {
     console.log(JSON.parse(result.toString()));
+    // NOTE: 여기선 카카오톡으로부터 받은 accessToken 등 데이터를 그냥 주었다. 각자의 프로젝트에 맞게 토큰을 생성해 커스텀하는건 본인 몫.
     return res.json({data: JSON.parse(result.toString())})
   })
 })
